19dccc89af28bb3dfe01c15c7556f665c486d228
ayushigoyal1 <132050137+ayushigoyal1@users.noreply.github.com>
2023-10-16 20:56:13 +0530

    Add files via upload

---
+++ fibnonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class fibnonacci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        int n1=0,n2=1;
+        System.out.println(n1+" ");
+        System.out.println(n2+"");
+        for(int i=2;i<n;i++){
+            int n3=n1+n2;
+            System.out.println(""+n3);
+            n1=n2;
+            n2=n3;
+            sc.close();
+        }
+        }
+}
